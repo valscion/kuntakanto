9ab693b8dbe493856a0d5ef12768f80ce53692db
Vesa Laakso <laakso.vesa@gmail.com>
2013-05-10 16:31:04 +0300

    Moved GameStateController to its own file

--- src/js/controller/Controllers.js
+++ src/js/controller/Controllers.js
@@ -117,24 +117,3 @@ dime.Controllers = {
     }
   }
 };
-
-// The controller responsible for changing between different game modes
-dime.GameStateController = function () {
-};
-
-dime.GameStateController.prototype = {
-  handlesKeyUp: function (keyName) {
-    // Only handle keys when game has not started or game is over
-    if (!dime.Game.status.gameStarted || dime.Game.status.gameOver) {
-      if (keyName === 'space')
-        return true;
-    }
-  },
-
-  keyUp: function (keyName) {
-    if (!dime.Game.status.gameStarted && !dime.Game.status.gameOver) {
-      // Start the game.
-      dime.Game.start();
-    }
-  }
-};
---
+++ src/js/controller/GameStateController.js
@@ -0,0 +1,24 @@
+// GameStateController.js
+// - The controller responsible for changing between different game modes
+
+'use strict';
+
+dime.GameStateController = function () {
+};
+
+dime.GameStateController.prototype = {
+  handlesKeyUp: function (keyName) {
+    // Only handle keys when game has not started or game is over
+    if (!dime.Game.status.gameStarted || dime.Game.status.gameOver) {
+      if (keyName === 'space')
+        return true;
+    }
+  },
+
+  keyUp: function (keyName) {
+    if (!dime.Game.status.gameStarted && !dime.Game.status.gameOver) {
+      // Start the game.
+      dime.Game.start();
+    }
+  }
+};
